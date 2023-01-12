bf403c8a05d507130e6e86f507a6cf1fba2c2203
leebach98 <44629665+leebach98@users.noreply.github.com>
2023-01-12 21:10:20 +0900

    웹페이지 이동 추가

--- Assets/Hue/Scripts/HTMLCrawling.cs
+++ Assets/Hue/Scripts/HTMLCrawling.cs
@@ -8,6 +8,7 @@ using System;
 public class HTMLCrawling : MonoBehaviour
 {
     string url = "https://lily.sunmoon.ac.kr/Page2/Story/Notice.aspx";
+        
 
     public GameObject notice;
     public GameObject noticePanel;
@@ -21,6 +22,10 @@ public class HTMLCrawling : MonoBehaviour
     public List<string> titleList;
     public List<string> authorList;
     public List<string> dateList;
+    public List<string> urlList;
+
+
+    Button titleBTN;
 
     void Start()
     {
@@ -34,45 +39,30 @@ public class HTMLCrawling : MonoBehaviour
         htmlDoc.LoadHtml(html);
 
 
-        //var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
-        //Debug.Log(node.Name);
-        //Debug.Log(node.OuterHtml);
-
-        //var node3 = htmlDoc.DocumentNode.SelectNodes("//button");
-        //Debug.Log(node3);
-
-        //foreach (var o in node3)
-        //{
-
-        //    Debug.Log("button class " + o.Attributes["class"].Value);
-
-        //}
-
-        //var node2 = htmlDoc.DocumentNode.SelectNodes("//body//h1");
-
-        //foreach (var i in node2)
-
-        //{
-
-        //    Debug.Log("1 " + i.InnerHtml);
-
-        //}
-
-        //foreach (var i in node2)
-
-        //{
-
-        //    Debug.Log("2" + i.OuterHtml);
-
-        //}
+      
 
         var htmlNodesTitle = htmlDoc.DocumentNode.SelectNodes("//body//div//td//a");
+        //var htmlNodesLink = htmlDoc.DocumentNode.SelectNodes("//body//div//td////a[@href]");
         var htmlNodesAuthor = htmlDoc.DocumentNode.SelectNodes("//body//div//td");
+
+
+        
 
         foreach (var i in htmlNodesTitle)
         {
             num += 1;
             titleList.Add(i.InnerHtml);
+            
+            //Debug.Log(i.OuterHtml.IndexOf(">"));
+
+            string summaryUrl = i.OuterHtml.Remove(0,9);
+            summaryUrl = summaryUrl.Substring(0,i.OuterHtml.IndexOf(">")-10);
+
+
+            urlList.Add("https://lily.sunmoon.ac.kr/"+summaryUrl);
+
+            
+            
         }
 
         foreach (var j in htmlNodesAuthor)
@@ -112,40 +102,25 @@ public class HTMLCrawling : MonoBehaviour
             title.text = titleList[i];
             date.text = dateList[i];
             author.text = authorList[i];
+
+
+            title.gameObject.name = i.ToString();
+
+            titleBTN = title.gameObject.AddComponent<Button>();
+
+            titleBTN.onClick.AddListener(() => SurfingUrl(Int32.Parse(titleBTN.gameObject.name)));
         }
-
-
         
 
-
-        //// span tag 중에 class 가 headline 인것
-        //var spanHead = htmlDoc.DocumentNode.SelectSingleNode("//div/input");
-        //var spanHead1 = htmlDoc.DocumentNode.SelectSingleNode("div/input");
-        //Debug.Log(spanHead.Name);
-        //Debug.Log(spanHead1.Name);
-
-
-
-
-
-        //// 전체 html 에서 a tag 중 "some_class_name" class 를 포함하는 첫번째 element
-        //var div = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'some_class_name')]");
-
-        //// 그 div 안에 table > tr > td > h3 > a 를 가져옴
-        //var a = div.SelectSingleNode("table/tr/td/h3/a");
-
-        //// a 의 href 속성을 가져옴. 두번째 파라메터는 속성이 없을 경우 기본값
-        //Debug.Log(a.GetAttributeValue("href", ""));
-        //// a 안에 있는 텍스트 가져오기
-        //Debug.Log(a.InnerText);
-        //// 여러개의 노드 조회
-        //var prdtImgs = htmlDoc.DocumentNode.SelectNodes("//img");
-        //foreach (var prdtImg in prdtImgs)
-        //{
-        //    Debug.Log(prdtImg.GetAttributeValue("src", ""));
-        //}
-
     }
+
+    void SurfingUrl(int i)
+    {
+        
+        //Debug.Log("");
+        Application.OpenURL(urlList[i]);
+    }
+
 
     // Update is called once per frame
     void Update()

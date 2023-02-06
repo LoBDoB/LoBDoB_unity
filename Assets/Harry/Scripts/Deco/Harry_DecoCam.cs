d15eb1802399d9665587479262f5cc1e00786af1
Jihoon <21qqq4@gmail.com>
2023-02-06 23:24:53 +0900

    물건 배치 구현

---
+++ Assets/Harry/Scripts/Deco/Harry_DecoCam.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harry_DecoCam : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float rotSpeed;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Third_Demen_Cam();
+    }
+
+    void Third_Demen_Cam()
+    {
+        Vector3 dir;
+        // WASD 키로 캠 위치 이동
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float mh = Input.GetAxis("Mouse X");
+        float mv = Input.GetAxis("Mouse Y");
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        dir = h * transform.right + v * transform.forward;
+        // 마우스 좌클릭으로 캠 위치 이동
+        if (Input.GetMouseButton(0))
+            dir = -mh * transform.right + -mv * transform.up;
+        transform.position += dir * speed * 5 * Time.deltaTime;
+        // 마우스 우클릭으로 캠 각도 이동
+        Vector3 rotDir = new Vector3(-mv, mh, 0);
+        if (Input.GetMouseButton(1))
+            transform.eulerAngles += rotDir * rotSpeed * 5 * Time.deltaTime;
+        // 스크롤로 확대
+        transform.position += scrollWheel * transform.forward * speed * 50 * Time.deltaTime;
+
+        transform.position = Vector3.Lerp(transform.position, transform.position, Time.deltaTime * 8.0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation, Time.deltaTime * 8.0f);
+    }
+}
---
+++ Assets/Harry/Scripts/Deco/Harry_PutObject.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harry_PutObject : MonoBehaviour
+{
+    Transform room;
+
+    public GameObject objFactory;
+    GameObject obj;
+    bool canPut = true;
+    public Material can;
+    public Material cant;
+    [SerializeField]
+    List<Material> origMats = new List<Material>();
+    Harry_ObjectCol objCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        room = GameObject.Find("Room").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThirdPut();
+    }
+
+    public void delObj()
+    {
+        if (obj)
+        {
+            Destroy(obj);
+            obj = null;
+        }
+    }
+    
+    void ThirdPut()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f, LayerMask.GetMask("Floor")))
+            {
+                obj = Instantiate(objFactory);
+                obj.transform.parent = transform;
+                objCol = obj.transform.GetChild(0).gameObject.AddComponent<Harry_ObjectCol>();
+                AddOrigMats();
+            }
+        }
+        else if (Input.GetKey(KeyCode.G) && obj)
+        {
+            canPut = !objCol.IsCollide;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f, LayerMask.GetMask("Floor")))
+            {
+                obj.transform.position = hit.point;
+                Vector3 angle = obj.transform.eulerAngles;
+                angle.z = 0;
+                angle.x = hit.normal.x;
+                obj.transform.eulerAngles = angle;
+            }
+            else
+            {
+                canPut = false;
+            }
+
+            ChangeMat(canPut);
+
+            if (Input.GetKey(KeyCode.Q))
+                obj.transform.Rotate(0, -100f * Time.deltaTime, 0);
+            else if (Input.GetKey(KeyCode.E))
+                obj.transform.Rotate(0, 100f * Time.deltaTime, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.G) && canPut && obj)
+        {
+            ChangeToOrigMat();
+            obj.GetComponentInChildren<Collider>().isTrigger = false;
+            obj.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+            obj.transform.parent = room;
+            obj = null;
+        }
+        else if (Input.GetKeyUp(KeyCode.G) && !canPut && obj)
+        {
+            Destroy(obj);
+            obj = null;
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f))
+            {
+                if (hit.transform.parent.CompareTag("Obj"))
+                {
+                    Destroy(hit.transform.parent.gameObject);
+                }
+            }
+        }
+    }
+
+    void ChangeMat(bool value)
+    {
+        Transform go = obj.transform.GetChild(0);
+        if (value)
+        {
+            for (int i = 0; i < go.childCount; i++)
+            {
+                Material[] mats = new Material[go.GetChild(i).GetComponent<Renderer>().materials.Length];
+
+                for (int j = 0; j < mats.Length; j++)
+                {
+                    mats[j] = can;
+                }
+
+                go.GetChild(i).GetComponent<Renderer>().materials = mats;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < go.childCount; i++)
+            {
+                Material[] mats = new Material[go.GetChild(i).GetComponent<Renderer>().materials.Length];
+
+                for (int j = 0; j < mats.Length; j++)
+                {
+                    mats[j] = cant;
+                }
+
+                go.GetChild(i).GetComponent<Renderer>().materials = mats;
+            }
+        }
+    }
+
+    void AddOrigMats()
+    {
+        origMats.Clear();
+        Transform go = obj.transform.GetChild(0);
+        for (int i = 0; i < go.childCount; i++)
+        {
+            foreach(Material mt in go.GetChild(i).GetComponent<Renderer>().materials)
+            {
+                origMats.Add(mt);
+            }
+        }
+    }
+
+    void ChangeToOrigMat()
+    {
+        Transform go = obj.transform.GetChild(0);
+        int idx = 0;
+        for (int i = 0; i < go.childCount; i++)
+        {
+            Material[] mats = new Material[go.GetChild(i).GetComponent<Renderer>().materials.Length];
+
+            for (int j = 0; j < mats.Length; j++)
+            {
+                mats[j] = origMats[idx++]; ;
+            }
+
+            go.GetChild(i).GetComponent<Renderer>().materials = mats;
+        }
+    }
+}

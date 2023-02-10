da0db83e3ed74e3f296455e17ef99b874096fd06
Jihoon <21qqq4@gmail.com>
2023-02-10 15:05:11 +0900

    앉기 모션

--- Assets/Harry/Scripts/Harry_AvatarController.cs
+++ Assets/Harry/Scripts/Harry_AvatarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 
 public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
 {
@@ -15,6 +16,7 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
     private Dictionary<string, bool> _Status = new Dictionary<string, bool>
     {
         {"Jump", false },
+        {"Sit", false },
     };
 
     void Start()
@@ -27,14 +29,15 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
     {
         if (photonView.IsMine)
         {
-            GRAVITY();
             STATUS();
 
             if (!_Status.ContainsValue(true))
             {
+                GRAVITY();
                 MOVE();
                 KEY_UP();
                 JUMP();
+                SIT();
             }
             else if (_Status.ContainsValue(true))
             {
@@ -49,8 +52,14 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
                 }
                 if (status_name == "Jump")
                 {
+                    GRAVITY();
                     MOVE();
                     JUMP();
+                }
+                if (status_name == "Sit")
+                {
+                    //MOVE();
+                    SIT();
                 }
             }
         }
@@ -60,6 +69,8 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
             transform.rotation = Quaternion.Lerp(transform.rotation, receiveRot, 15 * Time.deltaTime);
         }
     }
+
+
     //--------------------------------------------------------------------- STATUS
     private void STATUS()
     {
@@ -70,6 +81,14 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
         else if (!_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Jump"))
         {
             _Status["Jump"] = false;
+        }
+        if (_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Sit") || _Animator.GetCurrentAnimatorStateInfo(0).IsTag("Standing"))
+        {
+            _Status["Sit"] = true;
+        }
+        else if (!_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Sit") && !_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Standing"))
+        {
+            _Status["Sit"] = false;
         }
     }
     //--------------------------------------------------------------------- GRAVITY
@@ -109,27 +128,27 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
             photonView.RPC("RPCSetFloat", RpcTarget.All, "Speed", animSpeed);
         }
 
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (!_Animator.GetCurrentAnimatorStateInfo(0).IsName("jump"))
+            {
+                photonView.RPC("RPCCrossFade", RpcTarget.All, "move", 0.02f);
+            }
+        }
+
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 dir = h * Camera.main.transform.right + v * Camera.main.transform.forward;
         dir.y = 0;
         dir.Normalize();
 
-        if (dir.magnitude > 0.1f)
+        if (dir.magnitude > 0.1f && (_Animator.GetCurrentAnimatorStateInfo(0).IsName("move") || _Animator.GetCurrentAnimatorStateInfo(0).IsName("jump")))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10f);
 
             Vector3 velocity = this.transform.rotation * new Vector3(0, 0, speed);
             MOVE_XZ(velocity);
             MOVE_RESET();
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
-        {
-            if (!_Animator.GetCurrentAnimatorStateInfo(0).IsName("jump"))
-            {
-                photonView.RPC("RPCCrossFade", RpcTarget.All, "move", 0.05f);
-            }
         }
 
         //KEY_UP();
@@ -205,6 +224,38 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
         }
     }
 
+    Vector3 sitPos = Vector3.zero;
+    float dist;
+    private void SIT()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && !_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Sit") && canSit)
+        {
+            photonView.RPC("RPCCrossFade", RpcTarget.All, "sit", 0.1f); 
+            
+            Vector3 dir = chairPos - transform.position;
+            dir.y = 0;
+            Vector3 cross = Vector3.Cross(chairRot, Vector3.up);
+            cross.y = 0;
+
+            sitPos = chairPos - Vector3.Dot(dir, cross) * cross + chairSize.z * chairRot * 0.35f + chairSize.y * Vector3.up * 0.5f;
+            dist = 0.5f;
+        }
+        if (_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Sit")
+            && !_Animator.IsInTransition(0))
+        {
+            transform.position = Vector3.Lerp(transform.position, sitPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(chairRot), Time.deltaTime * 5);
+            if (Input.GetKeyDown(KeyCode.X))
+                photonView.RPC("RPCCrossFade", RpcTarget.All, "stand", 0.1f);
+        }
+        if (_Animator.GetCurrentAnimatorStateInfo(0).IsTag("Standing"))
+        {
+            GRAVITY();
+            dist = Mathf.Lerp(dist, 0, Time.deltaTime);
+            _Ctrl.Move(transform.forward * dist * Time.deltaTime);
+        }
+    }
+
     //도착 위치
     Vector3 receivePos;
     //회전되야 하는 값
@@ -239,5 +290,31 @@ public class Harry_AvatarController : MonoBehaviourPun, IPunObservable
     {
         if (_Animator != null)
             _Animator.CrossFade(stateName, normalizedTransitionDuration);
+    }
+
+    [SerializeField]
+    bool canSit;
+    Vector3 chairPos = Vector3.zero;
+    Vector3 chairSize = Vector3.zero;
+    Vector3 chairRot = Vector3.zero;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Chair"))
+        {
+            canSit = true;
+            BoxCollider col = (BoxCollider)other;
+            chairSize = col.size;
+            chairPos = other.transform.position;
+            chairRot = other.transform.forward;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Chair"))
+        {
+            canSit = false;
+        }
     }
 }

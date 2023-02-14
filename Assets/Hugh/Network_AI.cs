f043ecb9035c45c49429fbef9d329ec5b2c62043
leebach98 <44629665+leebach98@users.noreply.github.com>
2023-02-14 16:17:58 +0900

    asdf

--- Assets/Hugh/Network_AI.cs
+++ Assets/Hugh/Network_AI.cs
@@ -17,42 +17,40 @@ public class Network_AI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        UserInput userInput = new UserInput();
-
-        userInput.value = "설정";
-
-
-        string json = JsonUtility.ToJson(userInput);
-
-        Debug.LogError(json);
-
-
-        StartCoroutine(SendMessaget(json));
-
-        //StartCoroutine(Upload("http://192.168.50.215:5000/create",json));
-
+        ChangeJson("hi");
+       
+        
     }
 
-    IEnumerator UnityWebRequestGETTest()
+    //IEnumerator UnityWebRequestGETTest()
+    //{
+    //    // UnityWebRequest에 내장되있는 GET 메소드를 사용한다.
+    //    UnityWebRequest www = UnityWebRequest.Get(url);
+
+    //    yield return www.SendWebRequest();  // 응답이 올때까지 대기한다.
+
+    //    if (www.error == null)  // 에러가 나지 않으면 동작.
+    //    {
+    //        Debug.Log(www.downloadHandler.text);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("error");
+    //    }
+    //}
+
+    void ChangeJson(string text)
     {
-        // UnityWebRequest에 내장되있는 GET 메소드를 사용한다.
-        UnityWebRequest www = UnityWebRequest.Get(url);
-
-        yield return www.SendWebRequest();  // 응답이 올때까지 대기한다.
-
-        if (www.error == null)  // 에러가 나지 않으면 동작.
-        {
-            Debug.Log(www.downloadHandler.text);
-        }
-        else
-        {
-            Debug.Log("error");
-        }
+        UserInput userInput = new UserInput();
+        userInput.value = text;
+        string json = JsonUtility.ToJson(userInput);
+        StartCoroutine(SendMessaget(json));
     }
+
 
     IEnumerator SendMessaget(string json)
     {
+        //string json = JsonUtility.ToJson(userInput);
         WWWForm wWForm = new WWWForm();
 
         wWForm.AddField("username", json);
@@ -70,6 +68,29 @@ public class Network_AI : MonoBehaviour
         }
         www.Dispose();
     }
+
+
+    void JsonParsing(string json)
+    {
+        JObject jobject = JObject.Parse(json);
+        Debug.LogError(jobject.ToString());
+
+        // JSON 데이터 하위 객체인 members 객체의 name 값을 반복적으로 접근하는 방법
+        JToken jToken = jobject["username"];
+        Debug.LogError(jToken);
+
+
+       
+        foreach (JToken data in jToken)
+        {
+            Debug.LogError(data);
+            SearchRecommend searchRecommend = new SearchRecommend();
+
+            searchRecommend.value.Add(data.ToString());
+        }
+
+    }
+
 
 
     //IEnumerator Upload(string URL, string json)
@@ -98,25 +119,6 @@ public class Network_AI : MonoBehaviour
     //        request.Dispose();
     //    }
     //}
-
-
-    void JsonParsing(string json)
-    {
-        JObject jobject = JObject.Parse(json);
-        Debug.LogError(jobject.ToString());
-
-        // JSON 데이터 하위 객체인 members 객체의 name 값을 반복적으로 접근하는 방법
-        JToken jToken = jobject["username"];
-        Debug.LogError(jToken);
-
-
-       
-        foreach (JToken data in jToken)
-        {
-            Debug.LogError(data);
-        }
-
-    }
 
 }
 

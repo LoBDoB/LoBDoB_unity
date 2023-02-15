2800133924e8cd8859f4a57c0edff1610738d79a
leebach98 <44629665+leebach98@users.noreply.github.com>
2023-02-15 18:21:46 +0900

    강의추천]

--- Assets/Hugh/Network_AI.cs
+++ Assets/Hugh/Network_AI.cs
@@ -39,7 +39,7 @@ public class Network_AI : MonoBehaviour
     //    }
     
 
-    void ChangeJson(string text)
+    public void ChangeJson(string text)
     {
         //UserInput userInput = new UserInput();
         //userInput.value = text;
@@ -79,6 +79,8 @@ public class Network_AI : MonoBehaviour
         JToken jToken = jobject["searchResult"];
 
         JToken jToken1 = jobject["editWord"];
+
+        
 
         Debug.LogError(jToken);
         Debug.LogError(jToken1);
--- Assets/Hugh/RecommendClass.cs
+++ Assets/Hugh/RecommendClass.cs
@@ -3,18 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+
+using UnityEngine.UI;
+
 
 public class RecommendClass : MonoBehaviour
 {
+    //public RawImage img;
+
+    public Transform content;
+
+    public Texture[] lecture_State_IMG;
+
+    public InputField search;
+
+    public GameObject lecture;
+
+    public GameObject safari;
+
 
     string url = "http://192.168.50.55:8000/lecture_model/";
 
-    private void Start()
+    public List<string> lecture_Title = new List<string>();
+    public List<string> lecture_State = new List<string>();
+    public List<string> lecture_URL = new List<string>();
+    public List<string> lecture_Line = new List<string>();
+    public List<string> lecture_IMG_URL = new List<string>();
+
+
+
+    public void SearchField()
     {
-        StartCoroutine(SendMessaget("과."));
+        StartCoroutine(SendMessaget(search.text));
+        search.text = "";
     }
 
+    IEnumerator DownloadTexture(RawImage img, string url)
+    {
+        //Debug.LogError($"요청: {url}");
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("실패");
+            Debug.LogError(url);
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("imageDownload");
+            img.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
 
 
     IEnumerator SendMessaget(string json)
@@ -40,40 +80,62 @@ public class RecommendClass : MonoBehaviour
     }
 
 
-
-    public void JsonParsing(string json)
+    int cout = 0;
+    public void JsonParsing(string text)
     {
-        JObject jobject = JObject.Parse(json);
-        Debug.LogError(jobject.ToString());
-
-        // JSON 데이터 하위 객체인 members 객체의 name 값을 반복적으로 접근하는 방법
-        JToken jToken = jobject["searchResult"];
-
-        JToken jToken1 = jobject["editWord"];
-
-        Debug.LogError(jToken);
-        //Debug.LogError(jToken1);
-
-
-
-
-        var data = JsonConvert.DeserializeObject<UnitTest1>(json);
-        Debug.Log(data);
-
-    }
-}
-
-public class UnitTest1
-{
-    public class Data
-    {
-        public Infolist[] InfoList { get; set; }
-
-        public class Infolist
+        JObject json = JObject.Parse(text);
+        JToken jt = json["searchResult"];
+        Debug.LogError(jt);
+        foreach (JProperty j in jt)
         {
-            public string Name { get; set; }
-            public string Habby { get; set; }
-            public string Nmbr { get; set; }
+            //Debug.LogError("key name : " + j.Name);
+
+            cout += 1;
+
+            lecture_Title.Add(j.Name);
+            lecture_State.Add(j.Value["수강 가능 여부"].ToString());
+            lecture_Line.Add(j.Value["강좌 계열"].ToString());
+            lecture_URL.Add(j.Value["URL 강좌 링크"].ToString());
+            lecture_IMG_URL.Add(j.Value["이미지 url"].ToString());
+
+
+
+
+            RawImage showImage = Instantiate(lecture).GetComponent<RawImage>();
+
+            Button btn = showImage.gameObject.AddComponent<Button>();
+            btn.onClick.AddListener(() => SurfingUrl(j.Value["URL 강좌 링크"].ToString()));
+
+
+            if (j.Value["수강 가능 여부"].ToString() == "청강가능")
+            {
+                showImage.transform.GetChild(0).GetComponent<RawImage>().texture = lecture_State_IMG[1];
+            }
+            else
+            {
+                showImage.transform.GetChild(0).GetComponent<RawImage>().texture = lecture_State_IMG[0];
+            }
+ 
+            showImage.transform.SetParent(content);
+            StartCoroutine(DownloadTexture(showImage, j.Value["이미지 url"].ToString()));
+            
         }
+        cout = 0;
     }
+
+    public void SurfingUrl(string url)
+    {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        safari.SetActive(true);
+        WebViewScript webViewScript = safari.GetComponent<WebViewScript>();
+        webViewScript.StartWebView(url);
+        //webViewScript.webViewObject.LoadURL(url);
+
+#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_EDITOR_LINUX
+        Debug.Log(url);
+        Application.OpenURL(url);
+#endif
+
+    }
+
 }

a5f172098353343241539e1d7d657990958b44b4
leebach98 <44629665+leebach98@users.noreply.github.com>
2023-02-17 17:37:45 +0900

    mypage

--- Assets/Hugh/Scripts/OnOffFunction.cs
+++ Assets/Hugh/Scripts/OnOffFunction.cs
@@ -13,6 +13,7 @@ public class OnOffFunction : MonoBehaviour
     private void OnEnable()
     {
         checkBtn[0].gameObject.SetActive(true);
+        chartIMG[0].gameObject.SetActive(true);
         for (int i = 0; i < checkBtn.Count(); i++)
         {
             int k = i;
@@ -23,22 +24,23 @@ public class OnOffFunction : MonoBehaviour
         
     }
 
-
+    Color onClickColor;
+    Color offClickColor;
     public void CheckBTN(int num)
     {
         Debug.Log(num);
         
         for (int i = 0; i < checkBtn.Count(); i ++)
         {
-            checkBtn[i].color = Color.blue;
+            ColorUtility.TryParseHtmlString("#B5B5B5", out offClickColor);
+            checkBtn[i].color = offClickColor;
             chartIMG[i].gameObject.SetActive(false);
            
         }
         chartIMG[num].gameObject.SetActive(true);
-        checkBtn[num].color = Color.grey;
-        //chartIMG[num].gameObject.SetActive(false);
-
-
+        
+        ColorUtility.TryParseHtmlString("#C0A7D7", out onClickColor);
+        checkBtn[num].color = onClickColor;
 
     }
 }

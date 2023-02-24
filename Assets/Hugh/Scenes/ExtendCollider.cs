a8e29ae43f7e6e46587c0dee43cbcf1fce47cda0
leebach98 <44629665+leebach98@users.noreply.github.com>
2023-02-24 16:04:09 +0900

    asdf

--- Assets/Hugh/Scenes/ExtendCollider.cs
+++ Assets/Hugh/Scenes/ExtendCollider.cs
@@ -63,7 +63,14 @@ public class ExtendCollider : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         //Debug.LogError("31313");
-        
+        if (other.gameObject.name != "MainCameraView")
+        {
+            return;
+
+        }
+
+
+        //Debug.LogError(other.gameObject.name);
         if (onClick == true && extend == false)
         {
             RawImage forExtendScreen = other.gameObject.GetComponent<RawImage>();
